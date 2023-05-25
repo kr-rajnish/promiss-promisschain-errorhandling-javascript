e15ec45d29124bbca81c48d4d2295fd16e3cb176
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2023-05-25 14:10:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import "./styles.css";
 /*
 document.getElementById("app").innerHTML = `
-<h1>Promiss tutorial</h1> `;
+<h1>Promiss tutorial</h1> 
+<p>1. Promise can be created using a new Promise() constructor function.</br>
+// 2. This constructor function takes a callback function as argument. </br>
+// 3. The callback function has 2 arguments named 'resolve' and 'reject'. Resolve and reject are the keywords provided by JS.</br>
+// 4. We can only resolve or reject a promise. Nothing else can be done.</br>
+// 5. An error can also be created using new Error('error message').</br>
+// 6. There is also .catch() which is used to attach a failure callback function that handles any error that pops up during the execution of promise chain.</br>
+// 7. .catch only handles error of .then() that are present above it. If there is any .then() below it, catch will not handle any error for that, also that ,then will get executed no matter what.</br>
+// 8. It can be useful in a way if we want to catch error for a particular portion of a chain.</br>
+// 9. We can have multiple catch based on requirement and then a general catch at the end.</br>
+// 10. Always remember to return a value in the promise chain for the next .then to use .</br>
+// 11. If it returns a value => It will be  used as a parameter in next function. If it is a promise then the next .then in the promise chain is attached to the promise returned by the current callback function.</p>
+`;
 
 const cart = ["shoes", "pants", "kurta"];
 
